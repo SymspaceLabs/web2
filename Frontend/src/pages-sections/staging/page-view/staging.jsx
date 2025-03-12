c0c671102f39b67295091f303cebe1c7552b3129
Thilak Sundaram <techcrowdmy@gmail.com>
2025-03-12 11:38:38 +0800

    Updated Staging

--- Frontend/src/pages-sections/staging/page-view/staging.jsx
+++ Frontend/src/pages-sections/staging/page-view/staging.jsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import { BlobBox } from "@/components/BlobBox";
 import { FlexColCenter } from "@/components/flex-box";
 import Section1 from "../section-1";
 
@@ -6,11 +7,8 @@ export default async function StagingPageView() {
   return (
     <Box sx={{ position:'relative', overflow:'hidden', backgroundColor: '#fff' }}>
       {/* GRADIENT CIRCLES */}
-      <BlobBox top='1%' right={-150} />
-      {/* <BlobBox top='35%' left={250} />
-      <BlobBox top='45%' right={100} />
-      <BlobBox top='55%' left={100} color="#933FFE"/>
-      <BlobBox top='65%' right={-150} /> */}
+      {/* Blob 1: Hero Section Left */}
+      <BlobBox top="-5%" right="35%" background="#0366FE" widthHeight='750px' displayNoneMobile={true} /> {/* BLUE */}
 
       {/* CONTENT */}
       <FlexColCenter sx={{ width:'100%' }}>
@@ -18,27 +16,4 @@ export default async function StagingPageView() {
       </FlexColCenter>
     </Box>
   );
-}
-
-
-const BlobBox = ({top=null, right=null, bottom=null, left=null, color="#0366FE"}) => {
-  return(
-    <Box
-      sx={{
-        position: 'absolute',
-        top: top,
-        right: right,
-        bottom: bottom,
-        left: left,
-        width: '50vw',
-        height: '50vw',
-        maxWidth: '500px',
-        maxHeight: '500px',
-        borderRadius: '50%',
-        filter: 'blur(150px)',
-        opacity: 0.5,
-        backgroundColor: color,
-      }}
-    />
-  )
 }

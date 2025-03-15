c737de95a70fe4a41c4b3788e8926dd88c66b053
Thilak Sundaram <techcrowdmy@gmail.com>
2025-03-15 18:55:42 +0800

    Fix merge conflicts

--- Buyer/src/app/(layout-3)/marketplace/page.jsx
+++ Buyer/src/app/(layout-3)/marketplace/page.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import MarketplacePageView from "@/pages-sections/marketplace/page-view";
-=======
-import MarketplacePageView from "@/pages-sections/markeplace/page-view";
-
->>>>>>> f9102484ff15eb72e884a6aa8ee6512ed4354f68
 export const metadata = {
   title: "Marketplace",
   description: "Symspace is an E-commerce website.",

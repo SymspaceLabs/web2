d0a5218e62f0fe8b1328f0bec0f5ea8f6a887095
Thilak Sundaram <techcrowdmy@gmail.com>
2025-06-27 11:46:13 +0800

    Updated Backend

---
+++ Backend/src/data-source.ts
@@ -0,0 +1,15 @@
+// src/data-source.ts
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.HOST_DB,
+  port: parseInt(process.env.HOST_DB_PORT || '3306'),
+  username: process.env.DB_UNAME,
+  password: process.env.DB_UPASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+});

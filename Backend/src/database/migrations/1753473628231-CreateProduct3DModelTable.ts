f48b97c9b93b796c087ad6110054d65654f6dfa1
Thilak Sundaram <techcrowdmy@gmail.com>
2025-07-26 04:11:52 +0800

    Migration File

--- Backend/src/database/migrations/1753473628231-CreateProduct3DModelTable.ts
+++ Backend/src/database/migrations/1753473628231-CreateProduct3DModelTable.ts
@@ -1,23 +1,54 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
 
-export class CreateProduct3DModelTable1753473628231 implements MigrationInterface {
+export class CreateProduct3DModel1753473628231 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'product_3d_model',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            isUnique: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'url',
+            type: 'varchar',
+          },
+          {
+            name: 'colorCode',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'productId',
+            type: 'varchar',
+          },
+        ],
+      }),
+      true
+    );
 
-      public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            CREATE TABLE \`product_3d_model\` (
-                \`productId\` varchar(255) NOT NULL,
-                \`mediaId\` varchar(255) NOT NULL,
-                INDEX \`IDX_product\` (\`productId\`),
-                INDEX \`IDX_media\` (\`mediaId\`),
-                PRIMARY KEY (\`productId\`, \`mediaId\`),
-                CONSTRAINT \`FK_product\` FOREIGN KEY (\`productId\`) REFERENCES \`product\`(\`id\`) ON DELETE CASCADE,
-                CONSTRAINT \`FK_media\` FOREIGN KEY (\`mediaId\`) REFERENCES \`media\`(\`id\`) ON DELETE CASCADE
-            )
-        `);
+    await queryRunner.createForeignKey(
+      'product_3d_model',
+      new TableForeignKey({
+        columnNames: ['productId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'product',
+        onDelete: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('product_3d_model');
+    const foreignKey = table.foreignKeys.find(fk => fk.columnNames.includes('productId'));
+    if (foreignKey) {
+      await queryRunner.dropForeignKey('product_3d_model', foreignKey);
     }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE \`product_3d_model\``);
-    }
-
+    await queryRunner.dropTable('product_3d_model');
+  }
 }

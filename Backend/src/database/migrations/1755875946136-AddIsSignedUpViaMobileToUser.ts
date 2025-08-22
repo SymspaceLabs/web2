bf0277bf7325402be619d65facf14114def297f1
Thilak Sundaram <techcrowdmy@gmail.com>
2025-08-22 23:20:41 +0800

    Backend Migration FIle

---
+++ Backend/src/database/migrations/1755875946136-AddIsSignedUpViaMobileToUser.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AddIsSignedUpViaMobileToUser1755875946136 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "user",
+      new TableColumn({
+        name: "isSignedUpViaMobile",
+        type: "boolean",
+        default: false,
+        isNullable: false,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("user", "isSignedUpViaMobile");
+  }
+}

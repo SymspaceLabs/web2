e29efc90d9f6fd3e73dc95d02d6d593abe960cc3
Thilak Sundaram <techcrowdmy@gmail.com>
2025-12-30 08:52:19 +0800

    Updated appleLoginButton code

--- Buyer/src/components/header/components/AppleLoginButton.jsx
+++ Buyer/src/components/header/components/AppleLoginButton.jsx
@@ -12,37 +12,21 @@ const AppleSigninButton = () => {
   const [currentHost, setCurrentHost] = useState("");
 
   // ✅ This only runs on client, so window is safe
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     setCurrentHost(window.location.origin);
-
-  //     if (window.AppleID) {
-  //       window.AppleID.auth.init({
-  //         clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
-  //         scope: "email name",
-  //         redirectURI: process.env.NEXT_PUBLIC_APPLE_CALLBACK_URL, // or use currentHost
-  //         state: "state-value",
-  //         usePopup: true,
-  //       });
-  //     }
-  //   }
-  // }, []);
   useEffect(() => {
-    if (typeof window !== "undefined" && window.AppleID) {
-      window.AppleID.auth.init({
-        clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
-        scope: "email name",
-        // redirectURI: "https://www.symspacelabs.com/auth/apple/callback",
-        usePopup: true,
-      });
+    if (typeof window !== "undefined") {
+      setCurrentHost(window.location.origin);
+
+      if (window.AppleID) {
+        window.AppleID.auth.init({
+          clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
+          scope: "email name",
+          redirectURI: window.location.origin, // or use currentHost
+          state: "state-value",
+          usePopup: true,
+        });
+      }
     }
   }, []);
-
-  console.log(window.location.origin)
-  console.log(process.env.NEXT_PUBLIC_APPLE_CLIENT_ID)
-  console.log(process.env.NEXT_PUBLIC_APPLE_CALLBACK_URL)
-
-
 
   const handleAppleResponse = async (response) => {
     try {
